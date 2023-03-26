9b987a8f00758e9e93d36983e1124f6911b410bf
Drew Daniel <DrewTDaniel@gmail.com>
2023-03-26 18:51:57 -0400

    Added button for deleting gear item

--- frontend/src/components/Setup/SetupItem/SetupItem.jsx
+++ frontend/src/components/Setup/SetupItem/SetupItem.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import deleteIcon from '../../../assets/delete-button.png'
 import './SetupItem.scss';
+
+const deleteItem = (id) => {
+  console.log(id);
+}
 
 const SetupItem = ({ item }) => {
   const id = item.id;
   const { name, imageLink, link, body } = item.content;
 
+  const isAdmin = useSelector(state => state.auth.isAdmin);
+
+
+  const renderDelete = () => {
+    if (!isAdmin) {
+      return (
+        <div className="deleteButton">
+            <img src={ deleteIcon } alt='delete' onClick={() => deleteItem(id)} />
+        </div>
+      )
+    }
+  }
+
   return (
     <div className='setupItem'>
+      {renderDelete()}
       <div className='setupItem-image'>
         <img id='gearImage' src={imageLink} alt="missing"/>
       </div>
